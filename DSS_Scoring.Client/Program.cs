96d9863f0799c706f41694bb20e7c62f6588ded6
abeesp29 <abe.esp29@gmail.com>
2024-10-04 14:50:01 -0700

    abee1
    1.1

--- DSS_Scoring.Client/Program.cs
+++ DSS_Scoring.Client/Program.cs
@@ -4,4 +4,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 await builder.Build().RunAsync();
 
+//Abe 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7243/") });
+
 builder.Services.AddBlazorBootstrap();
